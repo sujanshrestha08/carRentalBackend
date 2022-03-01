93e29a9c95e926636f140f308a622ff2249c8b51
Sujan Shrestha <sujan.shrestha@neesum.com>
2022-03-01 14:10:29 +0545

    Completed Backend

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,41 +1,83 @@
-import express from 'express'
-import multer from 'multer'
-import path from 'path'
-const router = express.Router()
+// import express from 'express'
+// import multer from 'multer'
+// import path from 'path'
+// const router = express.Router()
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, 'uploads/')
+//   },
+//   filename(req, file, cb) {
+//     cb(
+//       null,
+//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+//     )
+//   },
+// })
+
+// function checkFileType(file, cb) {
+//   const filetypes = /jpg|jpeg|png/
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+//   const mimetype = filetypes.test(file.mimetype)
+
+//   if (extname && mimetype) {
+//     return cb(null, true)
+//   } else {
+//     cb('Images only!')
+//   }
+// }
+
+// const upload = multer({
+//   storage,
+//   fileFilter: function (req, file, cb) {
+//     checkFileType(file, cb)
+//   },
+// })
+
+// router.post('/', upload.single('image'), (req, res) => {
+//   res.send(`/${req.file.path}`)
+// })
+
+// export default router
+
+
+import express from "express";
+import multer from "multer";
+import path from "path";
+const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/')
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
   },
-  filename(req, file, cb) {
+
+  filename: function (req, file, cb) {
     cb(
       null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    )
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
   },
-})
+});
 
 function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-  const mimetype = filetypes.test(file.mimetype)
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
   if (extname && mimetype) {
-    return cb(null, true)
+    return cb(null, true);
   } else {
-    cb('Images only!')
+    cb("Images only!");
   }
 }
 
 const upload = multer({
   storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb)
-  },
-})
+});
 
-router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
-})
+router.post("/", upload.single("image"), (req, res) => {
+  // let fileUrl = req.file.path.replace(/\\/g, "/").substring("public".length);
+  res.send(`${req.file.filename}`);
+});
 
-export default router
+export default router;
